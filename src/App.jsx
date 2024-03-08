b95855b601bdf784502786a435a7659f1a5f1806
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-03-08 21:55:45 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import "./App.css";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
+
+function App() {
+  return (
+    <div className="profile-box">
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </div>
+  );
+}
+
+export default App;
